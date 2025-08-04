07a4a4f41693d9de8cb003c24fd70101a944cbab
Shady-91 <Shady_91@hotmail.it>
2025-08-04 19:39:18 +0200

    chiamata api

--- main.js
+++ main.js
@@ -9,13 +9,24 @@ console.log("Hello World");
 //Abbellire con CSS o Bootstrap
 //Inserire un bottone che al click faccia il fetch altre 10 mail(sostituendo le altre)
 
-let email = "https://flynn.boolean.careers/exercises/api/random/mail"
+const api_url_endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail'
+
+const resultEl = document.getElementById('MyButton')
+
+console.log(api_url_endpoint);
+
+axios.get(api_url_endpoint)
+      .then(response => {
+            console.log(response);
+
+      })
 
 function genera10Email() {
       const link = "https://flynn.boolean.careers/exercises/api/random/mail"
       const emailList = []
-      for (let i = 1; i <= 10; i++)
-            genera10Email.push(genera10Email[i]) =>
-      console.log(genera10Email);
+      for (let i = 1; i <= 10; i++) {
+            genera10Email.push(genera10Email[i])
+            console.log(genera10Email);
 
-}
+      }
+};

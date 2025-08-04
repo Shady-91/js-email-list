ac6dfaca3783f6a667f68a5cd266deca0118b649
Shady-91 <Shady_91@hotmail.it>
2025-08-04 19:19:53 +0200

    start

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hello World");
+
+//Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+//generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+//Bonus
+
+//Abbellire con CSS o Bootstrap
+//Inserire un bottone che al click faccia il fetch altre 10 mail(sostituendo le altre)
+
+let email = "https://flynn.boolean.careers/exercises/api/random/mail"
+
+function genera10Email() {
+      const link = "https://flynn.boolean.careers/exercises/api/random/mail"
+      const emailList = []
+      for (let i = 1; i <= 10; i++)
+
+}

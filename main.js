2bf9117e0140c034a9e55052e087629434d518ae
Shady-91 <Shady_91@hotmail.it>
2025-08-05 13:00:48 +0200

    add botton and finish

--- main.js
+++ main.js
@@ -1,37 +1,24 @@
-console.log("Hello World");
-
-//Attraverso l'apposita API di Boolean
-// https://flynn.boolean.careers/exercises/api/random/mail
-//generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-
-//Bonus
-
-//Abbellire con CSS o Bootstrap
-//Inserire un bottone che al click faccia il fetch altre 10 mail(sostituendo le altre)
-
 const api_url_endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 const emailListEl = document.getElementById('lista-email');
 const generaBtn = document.getElementById('MyButton');
 
-
-
-
-axios.get(api_url_endpoint)
-      .then(response => {
-            console.log(response);
-
-      })
-console.log(api_url_endpoint);
+//  collega il click del bottone alla funzione
+generaBtn.addEventListener('click', genera10Email);
 
 function genera10Email() {
-      const emailList = []
-      for (let i = 1; i <= 10; i++) {
+      emailListEl.innerHTML = ""; // pulisce la lista
+
+      for (let i = 0; i < 10; i++) {
             axios.get(api_url_endpoint)
                   .then(response => {
                         const email = response.data.response;
                         const li = document.createElement("li");
                         li.textContent = email;
+                        li.classList.add("list-group-item"); // stile Bootstrap
                         emailListEl.appendChild(li);
+                  })
+                  .catch(error => {
+                        console.error("Errore nella richiesta:", error);
                   });
       }
 }
